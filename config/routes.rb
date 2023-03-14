fdf92da9248d98252e157c48661985347ca68123
Joanna <astronautnemo@gmail.com>
2023-03-14 19:46:35 +0300

    completed code-along

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Route Creation with resource
+  resources :birds, only: [:index, :show]
+
+  # Manual Route Creation
+  # get "/birds", to: "birds#index"
+  # get "/birds/:id", to: "birds#show"
 end
